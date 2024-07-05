7a60198aa28ebb597203a03a2f6433f2a3f2d550
Noble Benjamin <nobleben2008@gmail.com>
2024-07-05 19:38:50 +0100

    Made an input for the Bill

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   const [personalRating, setPersonalRating] = useState(0)
   const [friendRating, setFriendRating] = useState(0)
   return (
-
+    <div>
+      <span>How much was the bill? <input type="text" value={billPrice} onChange={(e) => {
+        setBillPrice(Number(e.target.value))
+      }}/></span>
+    </div>
   )
 }
 
