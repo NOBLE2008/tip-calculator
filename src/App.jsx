eef36575813b0a819c243d74e7b83443e8edd226
Noble Benjamin <nobleben2008@gmail.com>
2024-07-05 19:13:11 +0100

    Made state for Personal Rating and Friend Rating Price

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [billPrice, setBillPrice] = useState(null)
+  const [billPrice, setBillPrice] = useState(0)
+  const [personalRating, setPersonalRating] = useState(0)
+  const [friendRating, setFriendRating] = useState(0)
   return (
 
   )
