0fb40674358353aead1c33f1feb7555f110fd5d4
Noble Benjamin <nobleben2008@gmail.com>
2024-07-06 18:02:04 +0100

    Added Reset Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [billPrice, setBillPrice] = useState(0)
-  const [personalRating, setPersonalRating] = useState(0)
-  const [friendRating, setFriendRating] = useState(0)
+  const [billPrice, setBillPrice] = useState(0);
+  const [personalRating, setPersonalRating] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
   return (
     <div>
-      <div>How much was the bill? <input type="text" value={billPrice} onChange={(e) => {
-        setBillPrice(Number(e.target.value))
-      }}/></div>
-      <div>How did you like the service? <select value={personalRating} onChange={(e) => {
-        setPersonalRating(Number(e.target.value))
-      }}>
-        <option value="0">Dissatisfied (0%)</option>
-        <option value="5">It was okay (5%)</option>
-        <option value="10">It was good (10%)</option>
-        <option value="20">Absolutely amazing! (20%)</option>
-      </select></div>
-      <div>How did your friends like the service? <select value={friendRating} onChange={(e) => {
-        setFriendRating(Number(e.target.value))
-      }}>
-        <option value="0">Dissatisfied (0%)</option>
-        <option value="5">It was okay (5%)</option>
-        <option value="10">It was good (10%)</option>
-        <option value="20">Absolutely amazing! (20%)</option>
-      </select>
+      <div>
+        How much was the bill?{" "}
+        <input
+          type="text"
+          value={billPrice}
+          onChange={(e) => {
+            setBillPrice(Number(e.target.value));
+          }}
+        />
       </div>
-      <h3>You pay ${(Math.round(billPrice+((((personalRating/100)*billPrice)+((friendRating/100)*billPrice))/2)))} (${billPrice} + ${((((personalRating/100)*billPrice)+((friendRating/100)*billPrice))/2)} tip)</h3>
+      <div>
+        How did you like the service?{" "}
+        <select
+          value={personalRating}
+          onChange={(e) => {
+            setPersonalRating(Number(e.target.value));
+          }}
+        >
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">It was okay (5%)</option>
+          <option value="10">It was good (10%)</option>
+          <option value="20">Absolutely amazing! (20%)</option>
+        </select>
+      </div>
+      <div>
+        How did your friends like the service?{" "}
+        <select
+          value={friendRating}
+          onChange={(e) => {
+            setFriendRating(Number(e.target.value));
+          }}
+        >
+          <option value="0">Dissatisfied (0%)</option>
+          <option value="5">It was okay (5%)</option>
+          <option value="10">It was good (10%)</option>
+          <option value="20">Absolutely amazing! (20%)</option>
+        </select>
+      </div>
+      <h3>
+        You pay $
+        {Math.round(
+          billPrice +
+            ((personalRating / 100) * billPrice +
+              (friendRating / 100) * billPrice) /
+              2
+        )}{" "}
+        (${billPrice} + $
+        {((personalRating / 100) * billPrice +
+          (friendRating / 100) * billPrice) /
+          2}{" "}
+        tip)
+      </h3>
+      <button onClick={() => {
+        setBillPrice(0);
+        setPersonalRating(0);
+        setFriendRating(0);
+      }}>Reset</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
